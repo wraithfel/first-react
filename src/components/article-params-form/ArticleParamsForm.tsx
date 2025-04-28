d63e77e36707632b83f69b87fc00f8d648f441b2
Nikita Lebedev <l3bedev.n@yandex.ru>
2025-04-28 21:44:31 +0300

    Finish ArticleForm

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -63,17 +63,97 @@ export const ArticleParamsForm = ({initialState, onApply, onReset}: ArticleParam
 		<>
 			<ArrowButton isOpen={isOpen} onClick={toggle} />
 			<aside
-				className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button
-							title='Сбросить'
-							type='reset'
-						/>
-						<Button title='Применить' type='submit' />
-					</div>
-				</form>
-			</aside>
-		</>
-	);
+				ref={rootRef}
+				className={clsx(styles.container, {
+					[styles.container_open] : isOpen
+				})}>
+				<form
+				 className={styles.form}
+				 onSubmit={handleSubmit}
+				 onReset={handleReset}
+				>
+				<Text weight={800} size={31} uppercase>
+            Задайте параметры
+          </Text>
+          <Spacing size={50} />
+
+          {/* Шрифт */}
+          <Text size={12} weight={800} uppercase>
+            Шрифт
+          </Text>
+          <Select
+            selected={formState.fontFamilyOption}
+            options={fontFamilyOptions}
+            placeholder="Выберите шрифт"
+            onChange={(opt) =>
+              setFormState((s) => ({ ...s, fontFamilyOption: opt }))
+            }
+          />
+          <Spacing size={30} />
+
+          {/* Размер шрифта */}
+          <RadioGroup
+            title="Размер шрифта"
+            name="fontSize"
+            options={fontSizeOptions}
+            selected={formState.fontSizeOption}
+            onChange={(opt) =>
+              setFormState((s) => ({ ...s, fontSizeOption: opt }))
+            }
+          />
+          <Spacing size={30} />
+
+          {/* Цвет шрифта */}
+          <Text size={12} weight={800} uppercase>
+            Цвет шрифта
+          </Text>
+          <Select
+            selected={formState.fontColor}
+            options={fontColors}
+            placeholder="Выберите цвет шрифта"
+            onChange={(opt) =>
+              setFormState((s) => ({ ...s, fontColor: opt }))
+            }
+          />
+          <Spacing size={30} />
+
+          <Separator />
+          <Spacing size={30} />
+
+          {/* Цвет фона */}
+          <Text size={12} weight={800} uppercase>
+            Цвет фона
+          </Text>
+          <Select
+            selected={formState.backgroundColor}
+            options={backgroundColors}
+            placeholder="Выберите цвет фона"
+            onChange={(opt) =>
+              setFormState((s) => ({ ...s, backgroundColor: opt }))
+            }
+          />
+          <Spacing size={30} />
+
+          {/* Ширина контента */}
+          <Text size={12} weight={800} uppercase>
+            Ширина контента
+          </Text>
+          <Select
+            selected={formState.contentWidth}
+            options={contentWidthArr}
+            placeholder="Выберите ширину"
+            onChange={(opt) =>
+              setFormState((s) => ({ ...s, contentWidth: opt }))
+            }
+          />
+          <Spacing size={50} />
+
+          <div className={styles.bottomContainer}>
+            <Button title="Сбросить" type="reset" />
+            <Button title="Применить" type="submit" />
+          </div>
+        </form>
+      </aside>
+    </>
+  );
 };
